555e46bc60b9315c92eb4a917ef5105065c14572
michael <michael8090@gmail.com>
2014-09-17 02:47:15 +0800

    change index.jade

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var Article = require('../models/article').Article;
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+    Article.find(function (err, articles) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.render('index', {
+                title: 'undefined.im',
+                articles: articles
+            });
+        }
+    });
 });
 
 module.exports = router;
